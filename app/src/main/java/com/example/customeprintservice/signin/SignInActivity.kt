f5f71949023dccaa296bb00183c3eb17c63de1bc
irisadmin <vaibhavnerle61@gmail.com>
2020-10-13 19:53:31 +0530

    SplashScreenActivity,SignInCompanyActivity,SignInActivity design complete

---
+++ app/src/main/java/com/example/customeprintservice/signin/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.customeprintservice.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.customeprintservice.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
